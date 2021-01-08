87fb0ae9c121bdb3c9ec0af47a6496bef816a060
Valerio Carcaterra <valerio.carcaterra@gmail.com>
2021-01-08 17:17:22 +0100

    Aggiunta classe di utils

--- src/main/java/controller/WebNotesController.java
+++ src/main/java/controller/WebNotesController.java
@@ -2,18 +2,17 @@ package controller;
 
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
+import utils.UtilsClass;
 
 
 @Controller(value = "/webnotes")
-public class WebNotesController {
+public class WebNotesController extends UtilsClass {
 
-    Logger logger = LoggerFactory.getLogger(this.getClass());
+
 
     @GetMapping(value = "/home")
     public ModelAndView home(
---
+++ src/main/java/utils/UtilsClass.java
@@ -0,0 +1,9 @@
+package utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UtilsClass {
+
+    public Logger logger = LoggerFactory.getLogger(this.getClass());
+}
