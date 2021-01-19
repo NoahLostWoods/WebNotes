9b218eb2dedd3cda46a0c5b62812bb94725ce4dc
Valerio Carcaterra <valerio.carcaterra@accenture.com>
2021-01-19 14:23:14 +0100

    Implementazione del servizio di POST - postNota

--- src/main/java/com/ap/WebNotes/controller/WebNotesController.java
+++ src/main/java/com/ap/WebNotes/controller/WebNotesController.java
@@ -3,8 +3,12 @@ package com.ap.WebNotes.controller;
 
 import com.ap.WebNotes.model.Nota;
 import com.ap.WebNotes.service.implementations.NoteServiceImpl;
+import com.ap.WebNotes.utils.enums.CodAzione;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import utils.UtilsClass;
@@ -12,27 +16,49 @@ import utils.UtilsClass;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/web-notes", produces = MediaType.APPLICATION_JSON_VALUE)
 public class WebNotesController extends UtilsClass {
 
     @Autowired
     private NoteServiceImpl noteService;
 
     @GetMapping("/notes")
-    public ResponseEntity<ModelAndView> getNota(
+    public ModelAndView getNota(
             @RequestParam(value = "mock", required = false, defaultValue = "false") Boolean mock
     ) {
 
         if (Boolean.TRUE.equals(mock)) {
             logger.info("Fine chiamata servizio home mock -> {}", mock);
             mav.setViewName("mockPage");
-            return ResponseEntity.ok(mav);
+            return mav;
         }
 
         logger.info("Inizio chiamata al servizio home");
         mav.setViewName("index");
         List<Nota> listaNote = noteService.getAll();
         mav.addObject("listaNote", listaNote);
-        return ResponseEntity.ok(mav);
+        mav.addObject("nota", new Nota());
+        return mav;
+    }
+
+    @RequestMapping(value = "/inserisci/notes", method = RequestMethod.POST)
+    public ModelAndView postNota(
+            @RequestParam("codAzione") CodAzione codAzione,
+            @RequestParam(value = "mock", required = false, defaultValue = "false") Boolean mock,
+            @Validated Nota nota,
+            BindingResult bindingResult
+    ) {
+        if (Boolean.TRUE.equals(mock)) {
+            logger.info("Fine chiamata servizio postNota, mock -> {}", mock);
+        }
+        logger.info("Inizio chiamata servizio postNota, codAzione -> {}", codAzione);
+        ModelAndView mav = new ModelAndView();
+        noteService.saveNota(nota);
+        List<Nota> listaNote = noteService.getAll();
+        mav.addObject("listaNote", listaNote);
+        mav.addObject("nota", new Nota());
+        mav.setViewName("index");
+        return mav;
     }
 
 
