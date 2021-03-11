84c1b03a0867caad2a479abda553d1a1eaf43232
Valerio Carcaterra <valerio.carcaterra@accenture.com>
2021-03-12 00:39:41 +0100

    Added function checkUserExsistence

--- src/main/java/com/ap/webnotes/controller/UserController.java
+++ src/main/java/com/ap/webnotes/controller/UserController.java
@@ -7,15 +7,11 @@ import com.ap.webnotes.model.Users;
 import com.ap.webnotes.resource.UsersResource;
 import com.ap.webnotes.service.implementations.UserServiceImpl;
 import com.ap.webnotes.utils.UtilsClass;
-import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
 
 import java.util.Collections;
 import java.util.List;
@@ -66,19 +62,35 @@ public class UserController extends UtilsClass {
         logger.info("Inizio chiamata servizio postUser");
 
         Users users = postUserFactory.dtoToModel(dto);
+
+        List<Users> getUser = userService.getAllUsers();
         if (users != null &&
                 !users.getUser().isEmpty() &&
                 !users.getPassword().isEmpty()) {
             try {
-                /*
-                Creare funzione di controllo di user già esistente.
-                 */
-                userService.saveUser(users);
-                return ResponseEntity.ok("OK");
+                if (!checkUserExsistence(getUser, users)) {
+                    userService.saveUser(users);
+                    return ResponseEntity.ok("Utente registrato con successo!");
+                } else {
+                    return ResponseEntity.ok("Utente già esistente");
+                }
             } catch (Exception e) {
                 return ResponseEntity.ok("KO");
             }
         }
         return null;
+    }
+
+
+    /**
+     * method that allow to verify if an user exist or none
+     * @param checkUser user from external source
+     * @param usersToInsert user insert from dto
+     * @return true if user exist or false if user not exist
+     */
+    private static boolean checkUserExsistence(List<Users> checkUser, Users usersToInsert) {
+
+        return checkUser != null && checkUser.stream()
+                .anyMatch(check -> check.getUser().contains(usersToInsert.getUser()));
     }
 }
